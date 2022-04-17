fce5317d22cee83cb06b5818bf64c9f2d8d93a8d
Mabson Vinicius <mabson@live.com>
2022-04-17 19:30:17 -0300

    fauna connect

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,22 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss'
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 export function SignInButton() {
-    const isUserLoggedIn = false;
 
-    return  isUserLoggedIn ? (
-        <button type='button' className={styles.signInButton}>
-            <FaGithub color='#04D361'/>
-            Mabson Vinicius
+    const { data: session } = useSession();
+
+
+    return session ? (
+        <button type='button' className={styles.signInButton} onClick={() => signOut()}>
+            <FaGithub color='#04D361' />
+            {session.user.name}
             <FiX color='#737380' className={styles.closeIcon} />
         </button>
     ) : (
-        <button type='button' className={styles.signInButton}>
-            <FaGithub color='#EBA417'/>
+        <button type='button' className={styles.signInButton} onClick={() => signIn('github')}>
+            <FaGithub color='#EBA417' />
             Sign in with Github
         </button>
     )
