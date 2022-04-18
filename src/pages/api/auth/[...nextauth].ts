710b42b857bac0a632dfb92d27299ee1f0fb4809
Mabson Vinicius <mabson@live.com>
2022-04-18 00:12:13 -0300

    stripe events

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from "../../../services/fauna";
+import { query as q } from 'faunadb'
 
 export default NextAuth({
     // Configure one or more authentication providers
@@ -15,4 +17,40 @@ export default NextAuth({
         }),
         // ...add more providers here
     ],
+    callbacks: {
+        async signIn({ user }) {
+            const { email } = user;
+
+            try {
+
+                await fauna.query(
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index('user_by_email'),
+                                    q.Casefold(email)
+                                )
+                            )
+                        ), q.Create(
+                            q.Collection('users'),
+                            { data: { email } }
+                        ),
+                        q.Get(
+                            q.Match(
+                                q.Index('user_by_email'),
+                                q.Casefold(email)
+                            )
+                        )
+                    )
+
+                )
+                return true;
+
+            } catch (error) {
+                return false;
+            }
+        },
+    },
+    secret: process.env.JWT_SECRET
 })
