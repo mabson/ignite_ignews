95c4f2eb80d25eff98cdfe10a723a39821e52ed0
Mabson Vinicius <mabson@spmmacbookti01.local>
2022-04-18 13:54:45 -0300

    finish ignews

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,27 @@
-import { SignInButton } from '../SignInButton'
-import styles from './styles.module.scss'
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { ActiveLink } from "../ActiveLink";
+import { SignInButton } from "../SignInButton";
+import styles from "./styles.module.scss";
 
 export function Header() {
-    return (
-        <header className={styles.headerContainer}>
-            <div className={styles.headerContent}>
-                <img src="/images/logo.svg" alt="ig.news" />
-                <nav>
-                    <a href="" className={styles.active}>Home</a>
-                    <a href="">Posts</a>                    
-                </nav>
-                <SignInButton/>
-            </div>
-        </header>
-    )
+  const { asPath } = useRouter();
+
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
+            <a>Posts</a>
+          </ActiveLink>
+        </nav>
+
+        <SignInButton />
+      </div>
+    </header>
+  );
 }
--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,37 +1,44 @@
-import { signIn, useSession } from 'next-auth/react'
-import { api } from '../../services/axios';
-import { getStripeJs } from '../../services/stripe-js';
-import styles from './styles.module.scss'
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { api } from "../../services/axios";
+import { getStripeJs } from "../../services/stripe-js";
+import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
-    priceId: string
+  priceId: string;
 }
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession();
+  const router = useRouter();
 
-    const { data: session } = useSession();
-
-    async function handleSubscribe() {
-        if (!session) {
-            signIn('github');
-            return;
-        }
-
-        try {
-            const response = await api.post('/subscribe');
-            const { sessionId } = response.data;
-            const stripe = await getStripeJs();
-            await stripe.redirectToCheckout({ sessionId });
-
-        } catch (err) {
-            alert(err.message)
-        }
+  async function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
     }
 
+    if (session.activeSubscription) {
+      router.push("/posts");
+      return;
+    }
 
-    return (
-        <button type="button" className={styles.subscribeButton} onClick={handleSubscribe}>
-            Subscribe now
-        </button>
-    )
+    try {
+      const response = await api.post("/subscribe");
+      const { sessionId } = response.data;
+      const stripe = await getStripeJs();
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe now
+    </button>
+  );
 }
-
